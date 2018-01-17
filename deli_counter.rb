daa9579b8f0100ddeba403ca08d3af109757b233
Kyle Harris <k_harris95@live.com>
2018-01-17 21:52:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 # Write your code here.
 def line(customers)
-  customers.size == 0 ? "The line is currently empty." : (puts customers)
-
+    if customers == 0
+      puts "The line is currently empty."
+    end
 end
