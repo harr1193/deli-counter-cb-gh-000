d17dc700fd09e4575659dd19b753d4955e0e1935
Kyle Harris <k_harris95@live.com>
2018-01-17 22:04:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ end
 
 def take_a_number(line, name)
   line << name
-  puts "Welcome, #{name}. You are number #{line.size} in line"
+  puts "Welcome, #{name}. You are number #{line.size} in line."
 end
