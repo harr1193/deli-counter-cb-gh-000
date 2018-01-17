c1a9636b9622cacffcfe18dd0f41feacb224239a
Kyle Harris <k_harris95@live.com>
2018-01-17 21:56:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,10 @@
 def line(customers)
     if customers.size == 0
       puts "The line is currently empty."
+    else
+      line_str = "The line is currently: "
+      customers.each_with_index do |name, i|
+        line_str << "#{i+1}. #{name} "
+      end
     end
 end
